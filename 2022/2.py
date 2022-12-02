8d574e1a004b908a23a77e460aa65ab1bc6a7053
flowernal <majojo992@gmail.com>
2022-12-02 22:43:08 +0100

    🧹 Remove redundant parentheses

--- 2022/2.py
+++ 2022/2.py
@@ -1,4 +1,4 @@
 input = [game.split() for game in open("2.in", "r").read().splitlines()]
 
 print(f"Part 1: {sum([ord(m) - 87 + (ord(m) - 88 - (ord(o) - 66)) % 3 * 3 for o, m in input])}")
-print(f"Part 2: {sum([3 * (ord(m) - 88) + ((ord(m) - 87 + ord(o) - 64) % 3 + 1) for o, m in input])}")
+print(f"Part 2: {sum([3 * (ord(m) - 88) + (ord(m) - 87 + ord(o) - 64) % 3 + 1 for o, m in input])}")
