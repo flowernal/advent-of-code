0e63a27282cd8ca5daa76f66e75742abd9c8c752
flowernal <majojo992@gmail.com>
2022-12-03 13:47:35 +0100

    🎨 Change split parameters

--- 2022/1.py
+++ 2022/1.py
@@ -1,4 +1,4 @@
-input = [sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split("\n\n")]
+input = [sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))]
 
 print(f"Part 1: {max(input)}")
 print(f"Part 2: {sum(sorted(input, reverse=True)[:3])}")
