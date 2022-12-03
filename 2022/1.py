46675a24adccedd27757d370b440d0c8e0205000
flowernal <majojo992@gmail.com>
2022-12-03 13:54:04 +0100

    ✨ Update Day 1

--- 2022/1.py
+++ 2022/1.py
@@ -1,7 +1,7 @@
-input = [sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))]
+input = sorted([sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))])
 
-print(f"Part 1: {max(input)}")
-print(f"Part 2: {sum(sorted(input, reverse=True)[:3])}")
+print(f"Part 1: {input[-1]}")
+print(f"Part 2: {sum(input[-3:])}")
 
 # One-Liner
-# print(f'Part 1: {max([sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))])}\nPart 2: {sum(sorted([sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))], reverse=True)[:3])}')
+# print(f'Part 1: {sorted([sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))])[-1]}\nPart 2: {sum(sorted([sum(map(int, i.splitlines())) for i in open("1.in", "r").read().split(2 * chr(10))])[-3:])}')
