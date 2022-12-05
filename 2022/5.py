b23c43a47f942b40f03630e345fe84070b192a23
flowernal <majojo992@gmail.com>
2022-12-05 19:16:03 +0100

    ✨ Fix Day 5

--- 2022/5.py
+++ 2022/5.py
@@ -7,9 +7,9 @@ stack_strings = input[0][:-1][::-1]
 def part(part: int) -> str:
 	stacks = [[stack[i] for stack in stack_strings if stack[i] != ' '] for i in range(1, len(stack_strings[0]), 4)]
 
-	for amount, source, destination in [[int(n) for n in i.split()[1::2]] for i in input[1]]:
-		stacks[destination] += stacks[source][-amount:][::-1] if part == 1 else stacks[source][-amount:]
-		stacks[source] = stacks[source][:-amount]
+	for amount, source, destination in [[int(n) for n in instruction.split()[1::2]] for instruction in input[1]]:
+		stacks[destination - 1] += stacks[source - 1][-amount:][::-1] if part == 1 else stacks[source - 1][-amount:]
+		stacks[source - 1] = stacks[source - 1][:-amount]
 
 	return ''.join([stack[-1] for stack in stacks])
 
