9e07a039d960d8d1d811c6870ffd07da8a532770
flowernal <majojo992@gmail.com>
2022-12-05 17:52:58 +0100

    ✨ Optimize Day 5

--- 2022/5.py
+++ 2022/5.py
@@ -1,11 +1,13 @@
 from aoc import day
 
 input = [l.splitlines() for l in day(5).split(2 * chr(10))]
+
+crates_reversed = input[0][:-1][::-1]
 instructions = [[int(n) for n in i.split()[1::2]] for i in input[1]]
 
 
 def part(part: int) -> str:
-	crates = [[c[i] for c in input[0] if c[i] != ' '][:-1][::-1] for i in range(1, len(input[0][0]), 4)]
+	crates = [[crate[i] for crate in crates_reversed if crate[i] != ' '] for i in range(1, len(input[0][0]), 4)]
 
 	for i in instructions:
 		crates[i[2] - 1] += crates[i[1] - 1][-i[0]:][::-1] if part == 1 else crates[i[1] - 1][-i[0]:]
